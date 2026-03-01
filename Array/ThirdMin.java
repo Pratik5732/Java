6282f9640f65ca4c60c3ffa1d1abd33ab5b91248
Pratik Pawar <pratikspawar7992@gmail.com>
2026-03-02 02:47:23 +0530

    ThirdMin.java

---
+++ Array/ThirdMin.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class thirdmin
+{
+    public static void main(String x[])
+    {
+      int a[]=new int[5];
+      int temp;
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter the elements");  
+      for(int i=0;i<a.length;i++)
+      {
+        a[i]=sc.nextInt();
+      }
+      System.out.println("The elements is ");
+      for(int i=0;i<a.length;i++)
+      {
+        System.out.println(a[i]);
+      }
+      for(int i=0;i<a.length;i++)
+      {
+        for(int j=i+1;j<a.length;j++)
+        {
+           if(a[i]>a[j])
+           { 
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+           }
+        }
+      }
+      System.out.println("ascending element is");
+      for(int i=0;i<a.length;i++)
+      {
+        System.out.println(a[i]);
+      }
+      System.out.println("third min is " +a[2]);  
+    }
+}
