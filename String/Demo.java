901bf1846d324c2647a94bcfbf5cc0b659a5ebe0
Pratik Pawar <pratikspawar7992@gmail.com>
2026-03-02 23:59:57 +0530

    Demo.java

---
+++ String/Demo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class demo 
+{
+    public static void main(String x[])
+    {
+        String s;
+        System.out.println("Enter the string");
+        Scanner sc = new Scanner(System.in);
+        s=sc.nextLine();
+        System.out.println("The string is "+s);
+    }
+}
