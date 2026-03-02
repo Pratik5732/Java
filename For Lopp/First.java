f5a5b6052e007b1a5f1b3ddd859f428de5b6ff43
Pratik Pawar <pratikspawar7992@gmail.com>
2026-03-03 00:58:52 +0530

    First.java

---
+++ For Lopp/First.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class first
+{
+    public static void main(String x[])
+    {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println("Hello India");
+        }
+    }
+}
