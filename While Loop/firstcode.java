0fc0f291c5e2added6fb383133309649bb5897ce
Pratik Pawar <pratikspawar7992@gmail.com>
2026-03-03 01:20:29 +0530

    FirstCode.java

---
+++ While Loop/firstcode.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class first
+{
+    public static void main(String x[])
+    {
+        int n;
+        int i=1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the no");
+        n=sc.nextInt();
+        while(i<=n)
+        {
+            System.out.println("Good Morning");
+                    i++;
+        }
+    }
+}
