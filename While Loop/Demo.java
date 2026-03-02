32cf9f5752f07b94da248dec2c19306618761329
Pratik Pawar <pratikspawar7992@gmail.com>
2026-03-03 01:18:47 +0530

    Demo.java

---
+++ While Loop/Demo.java
@@ -0,0 +1,10 @@
+public class LocalExample2 {
+    public static void main(String[] args) {
+        showMessage();
+    }
+
+    public static void showMessage() {
+        String msg = "Hello, Java!"; // local variable
+        System.out.println(msg);
+    }
+}
