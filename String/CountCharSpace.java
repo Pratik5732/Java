134706986326a90adb00843e3ebb91ead0e661c4
Pratik Pawar <pratikspawar7992@gmail.com>
2026-03-03 00:03:26 +0530

    CountCharSpace.java

---
+++ String/CountCharSpace.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class countcharspace 
+{
+    public static void main(String x[])
+    {
+        String s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string ");
+        s=sc.nextLine();
+        int l=s.length();
+        System.out.println("The count of string is "+l);
+    }
+}
